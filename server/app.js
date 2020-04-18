29b009bee8659c42eada512bd587619af919b54c
Max Ross <maxymus.ross@gmail.com>
2020-04-17 21:08:58 -0600

    add some functionality

--- server/app.js
+++ server/app.js
@@ -17,62 +17,95 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
+
+
+app.get('/',
+  (req, res) => {
+    // console.log(req.body);
+    res.render('index');
+  });
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
-
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
+app.post('/signup', (req, res) => {
+  console.log(req.body);
+  if (models.Users.get({username: req.body.username})) {
+    //alert('ERROR: username already exists')
+    console.log('user already exists', req.body.username);
+    res.redirect('/signup');
   }
+  models.Users.create(req.body);
+  // .then(user => {
+  //   console.log('success!')
+  //   res.status(201);
+  // })
+  // .catch(error => {
+  //   console.log(error);
+  // })
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
-      });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
+
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,3 +8,13 @@ module.exports.createSession = (req, res, next) => {
 // Add additional authentication middleware functions below
 /************************************************************/
 
+module.exports.checkSession = (req, res, next) => {
+
+  cookie = req.cookies.sessionId;
+// We have the session hash from the cookie parser
+// Check sessions table to see if hash exists in db
+  // If yes,
+  // Load index view with user data
+  // Else
+  // Send them to login page
+};
